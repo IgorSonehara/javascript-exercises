97c36e764c38eb1ec135cc277cb53b9bce770d4d
Igor Sonehara <igormeche@gmail.com>
2023-03-18 22:20:47 -0300

    First 07 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(array, ...element) {
+  const arraySize = array.length
+    for (let i = 0; i < arraySize; i++) {
+      const index = array.indexOf(element[i]);
+      if (index > -1) {
+        const index = array.indexOf(element[i]);
+        array.splice(index, 1);
+      }
+    }
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    for (i = start; i <= end; i++) {
+        sum = sum + i;
+    }
+    return sum;
+//sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
 };
 
 // Do not edit below this line
